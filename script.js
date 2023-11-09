73ba829244065eec0df49cf9691fedbf82150c70
Santhosh <spinisetti6@gmail.com>
2023-11-09 15:46:55 +0530

    commit gsap2

---
+++ script.js
@@ -0,0 +1,25 @@
+gsap.from("#nav>img, #nav h3, #nav h4, #nav button",{
+    // y:-100,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:0.5,
+})
+// gsap.from("#nav",{
+//     // y:-100,
+//     duration:1,
+//     delay:1,
+//     opacity:0,
+// })
+var tl = gsap.timeline();
+tl.from("#main h1",{
+    opacity:0,
+    y:100,
+    delay:4,
+    stagger:0.5,
+})
+tl.from("#main>img",{
+    scale:0,
+    opacity:0,
+    stagger:0.5,
+})
